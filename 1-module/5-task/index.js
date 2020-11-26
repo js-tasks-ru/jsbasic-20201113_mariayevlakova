35c1f813874096e31f3e8482a7b6ef5ed8ab3ad6
Mariia Yevlakova <mariayevlakova@gmail.com>
2020-11-26 17:26:40 +0200

    truncate string

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -6,4 +6,10 @@
  */
 function truncate(str, maxlength) {
   // ваш код...
+  if (str.length <= maxlength) {
+    return str;
+  } else if (str.length > maxlength) {
+    return str.slice (0 ,maxlength -1) + '…';
+  }
+
 }
